aa80ebc6776bd2b29a54d57f639beefdf3e2012b
kgy-readin <kgy.readin@gmail.com>
2026-06-13 01:45:24 +0900

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const SPREADSHEET_ID = import.meta.env.VITE_SPREADSHEET_ID || "1xIlvxq4h1riV2BucwMKjcE2wmqm64xir2r_KyFTdp94";
-export const GAS_WEBAPP_URL = import.meta.env.VITE_GAS_WEBAPP_URL || "https://script.google.com/macros/s/AKfycbzNF3NzKFvJDskmIm2gF0Q-hIcSS0E09P71s2WvLCsli-jxOGde_EvdwB2Rf2AnLjQo8A/exec";
+export const SPREADSHEET_ID = import.meta.env.VITE_SPREADSHEET_ID || "";
+export const GAS_WEBAPP_URL = import.meta.env.VITE_GAS_WEBAPP_URL || "";
 export const APP_VERSION = "Logos 3.2.0";
